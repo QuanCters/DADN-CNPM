ad04387a133bcd30bdd1afac2cd89c3a99a49d09
TuanNguyenas <165824612+TuanNguyenas@users.noreply.github.com>
2024-11-25 00:12:55 +0700

    Add untracked files

---
+++ Documents/workspace/aitee_server-main 2/src/middlewares/index.js
@@ -0,0 +1,5 @@
+export { default as rateLimiter } from './rate-limiter.js';
+export { default as auth } from './auth/check-auth.js';
+export { default as imageUpload } from './image-upload.js';
+
+export { checkAdmin } from './auth/check-authority.js';
---
+++ Documents/workspace/aitee_server-main 2/src/routes/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import { serve, setup } from 'swagger-ui-express';
+
+import { specs, swaggerConfig } from '../config/index.js';
+
+import user from './user.js';
+import comment from './comment.js';
+import friend from './friend.js';
+
+const router = express.Router();
+
+const specDoc = swaggerJsdoc(swaggerConfig);
+router.use(specs, serve);
+router.get(specs, setup(specDoc, { explorer: true }));
+
+router.use('/user', user);
+router.use('/comment', comment);
+router.use('/friend', friend);
+
+export default router;
