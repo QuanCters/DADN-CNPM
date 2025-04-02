4052f9ccbe5a3788dd2d4219733bdc66f9e1e66f
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-04-03 03:01:22 +0700

    add sendMessage to Adafruit

--- backend/src/utils/ConnectDevice.js
+++ backend/src/utils/ConnectDevice.js
@@ -6,23 +6,46 @@ const AIO_BASE_URL = process.env.AIO_BASE_URL;
 const HEADERS = { "X-AIO-Key": AIO_KEY };
 
 const controlDevice = async (deviceKey, value) => {
-    const response = await axios.post(
-        `${AIO_BASE_URL}/${AIO_USERNAME}/feeds/${deviceKey}/data`,
-        { value },
-        { headers: HEADERS }
-    );
-    return response.data;
+  const response = await axios.post(
+    `${AIO_BASE_URL}/${AIO_USERNAME}/feeds/${deviceKey}/data`,
+    { value },
+    { headers: HEADERS }
+  );
+  return response.data;
 };
 const getDeviceStatus = async (deviceKey) => {
-  const response = await axios.get(
-    `${AIO_BASE_URL}/${AIO_USERNAME}/feeds/${deviceKey}`,
-    { headers: HEADERS }
-  )
-  .catch((err) => {
-    console.error(err);
-    throw err;
-  });
+  const response = await axios
+    .get(`${AIO_BASE_URL}/${AIO_USERNAME}/feeds/${deviceKey}`, {
+      headers: HEADERS,
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
 
   return response.data.last_value;
 };
-module.exports = { controlDevice, getDeviceStatus };
+
+const sendMessage = async (username, feed, aioKey, value) => {
+  const response = await fetch(
+    `https://io.adafruit.com/api/v2/${username}/feeds/${feed}/data?x-aio-key=${aioKey}`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        value: value,
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new BadRequestError("Error update status to Adafruit");
+  }
+
+  return await response.json();
+};
+
+module.exports = { controlDevice, getDeviceStatus, sendMessage };
