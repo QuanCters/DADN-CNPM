627e5dfd5f1f3286af58d0e3e0f9a28ee8561232
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-04-06 03:32:56 +0700

    add fcm

---
+++ frontend/services/fcm.service.ts
@@ -0,0 +1,53 @@
+import messaging from "@react-native-firebase/messaging";
+import { PermissionsAndroid } from "react-native";
+
+class FCMService {
+  currentToken: null | string;
+  constructor() {
+    this.currentToken = null;
+  }
+
+  async init(userId: Number) {
+    await this._requestPermission();
+    await this._registerToken(userId);
+    this._setupListener();
+  }
+
+  async _requestPermission() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+    );
+  }
+
+  async _registerToken(userId: Number) {
+    try {
+      console.log("HELLO");
+      this.currentToken = await messaging().getToken();
+      console.log(this.currentToken);
+    } catch (error) {
+      console.error("Lỗi đăng ký FCM Token:", error);
+    }
+  }
+
+  _setupListener() {
+    messaging().onMessage(async (remoteMessage) => {
+      console.log(remoteMessage);
+    });
+
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      this._handleNotificationClick(remoteMessage);
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) this._handleNotificationClick(remoteMessage);
+      });
+  }
+
+  _handleNotificationClick = (remoteMessage: any) => {
+    console.log("Notification Clicked:", remoteMessage);
+  };
+}
+
+export default new FCMService();
